b3479df34e2bf10c535040aa12a64bc8300b1995
Gabriela411 <gabriela.toncu@yahoo.com>
2024-01-27 17:47:55 +0000

    Convert to .jsx and add key
    Fixed the "Each child in a list should have a unique 'key' prop" by adding a unique key

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,12 +1,17 @@
 import './card-list.styles.css'
-import Card from '../card/card.component.jsx';
+import Card from '../card/card.component';
+import { Monster } from '../../App'
+
+type CardListProps = {
+    monsters: Monster[]; //an array of Monster objects
+}
 
 //props= object that has all diff parameters and properties that are being passed inside a component like CardList
-const CardList = ({ monsters }) => (
+const CardList = ({ monsters }: CardListProps) => (
     <div className='card-list'>
         {monsters.map((monster) => {
             return (
-                <Card monster={monster} />
+                <Card monster={monster} key={monster.id}/>
             )
         }
         )}
