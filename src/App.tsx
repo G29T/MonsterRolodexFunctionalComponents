5c9c8b56480115244614c52e6fa89b6db6f7d6cf
Gabriela411 <gabriela.toncu@yahoo.com>
2024-01-27 17:47:08 +0000

    Convert App.js into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/serach-box.component';
+import { getData } from './utils/data.utils';
 
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
 
 const App = () => {
 
   const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users)
-      );
+    const fetchUsers = async () => {
+      //const users = await getData<Array<Monster>>('https://jsonplaceholder.typicode.com/users')
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users');
+      setMonsters(users);
+    }
+
+    fetchUsers();
   }, []);
 
   useEffect(() => {
@@ -26,7 +35,7 @@ const App = () => {
   }, [monsters,searchField]);
 
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   }
@@ -34,7 +43,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-      <SearchBox className='monsters-search-box' searchedField={searchField} onChangeHandler={onSearchChange} />
+      <SearchBox className='monsters-search-box' placeholder={searchField} onChangeHandler={onSearchChange} />
       <CardList monsters={filteredMonsters} />
     </div>
   );
